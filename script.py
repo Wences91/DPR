58570d6051cc4db3ce79aa54df24b1509630b550
Wences <36030851+Wences91@users.noreply.github.com>
2018-12-05 12:23:19 +0100

    Descarga de tweets

--- script.py
+++ script.py
@@ -7,7 +7,7 @@ import tweepy
 # Archivo de salida
 salida = open('tuits.txt', 'w')
 
-# Cuenta de Twitter de la que descargar tuits
+# Cuenta de Twitter de la que descargar tweets
 twitter = 'CUENTA_DE TWITTER'
 
 # Datos de acceso a la API
@@ -15,7 +15,7 @@ auth = tweepy.OAuthHandler('API key', 'API secret key')
 auth.set_access_token('Access token', 'Access token secret')
 main_api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
 
-# Descarga de tuits
+# Descarga de tweets
 for friendCursor in tweepy.Cursor(main_api.user_timeline, tweet_mode='extended', trim_user='false', include_rts='false', count=200, screen_name=twitter).items():
     salida.write(friendCursor._json['full_text'] + '\n')
 
